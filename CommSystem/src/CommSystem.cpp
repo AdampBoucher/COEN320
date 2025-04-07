76170d64cdb9ab86d6440a3e78edec9401d2bce8
Adam Boucher <adampboucher@gmail.com>
2025-04-07 14:53:40 -0400

    added some more projects

---
+++ CommSystem/src/CommSystem.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>     // for sleep()
+#include <time.h>
+#include <iostream>
+#include <string>
+
+int main(void) {
+	puts("Hello world!");
+
+}
